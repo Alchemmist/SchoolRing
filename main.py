46c42820a01466d2deaf3795a1c9217ebb7e8cc9
user36672 <a>
2022-10-28 18:58:12 +0300

    i ustal

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import sys
 
-# Press Ctrl+F1 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-# Press the green button in the gutter to run the script.
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.number = ''
+        uic.loadUi('schoolring2.0.ui', self)
+        self.login_button_cerkle.clicked.connect(self.operation)
+
+    def operation(self):
+        print(0)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.exit(app.exec())
