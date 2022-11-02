70789ee9fb0a60feb8dca667e68e02d1dbd598d8
Anthropophag <anton.ingrish@gmail.com>
2022-11-02 21:06:26 +0300

    start do intaractive menu add directory with documentation

--- main.py
+++ main.py
@@ -9,10 +9,32 @@ class Window(QMainWindow):
         super().__init__()
         self.number = ''
         uic.loadUi('mu_ui_test.ui', self)
-        self.login_button_cerkle.clicked.connect(self.operation)
 
-    def operation(self):
-        print(0)
+        # menu tub button connect
+        self.init_menu_tub_button()
+        # menu logining button connect
+        self.init_logining_button()
+
+    def init_menu_tub_button(self):
+        self.home_button.clicked.connect(self.go_home)
+        self.timetable_button.clicked.connect(self.go_timetable)
+        self.timetable_button.clicked.connect(self.go_timetable)
+
+    def init_logining_button(self):
+        self.login_button_cerkle.clicked.connect(self.start_login)
+        self.login_button_text.clicked.connect(self.start_login)
+
+    def go_home(self):
+        self.tabs.setCurrentWidget(self.Home_page)
+
+    def go_timetable(self):
+        self.tabs.setCurrentIndex(1)
+
+    def go_template(self):
+        self.tabs.setCurrentIndex(2)
+
+    def start_login(self):
+        pass
 
 
 if __name__ == '__main__':
